96ce476a7757c5ef4f695a0a3836550fe2fc7572
Michalskii <gorynski.michal@gmail.com>
2022-02-14 19:23:54 +0100

    store searches

--- app.js
+++ app.js
@@ -4,3 +4,28 @@ const navUl = document.getElementById('nav-ul');
 hamburger.addEventListener('click', () => {
     navUl.classList.toggle('show');
 });
+
+let search = [];
+
+
+
+
+ document.addEventListener('DOMContentLoaded', ()=>{
+            document
+            .getElementById('btn')
+            .addEventListener('click', addSearched);
+        }
+        );
+const addSearched = (ev) => {
+
+let searched = {
+    location: document.getElementById('location').value,
+    propertyType: document.getElementById('property-type').value,
+    maxPrice: document.getElementById('max-price').value
+
+}
+search.push(searched);
+console.log(searched)
+}
+  
+
