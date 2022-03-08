08f1e285f2524ab35267e46e8fbfaba8c471c7a6
Michalskii <94405347+Michalskii@users.noreply.github.com>
2022-03-08 21:34:00 +0100

    Validation proba 2

--- script.js
+++ script.js
@@ -9,77 +9,103 @@ $('#menu_checkbox').click(function(){
  let search = [];
          
      
-const selectLocation = document.getElementById('location');
+let selectLocation = document.getElementById('location');
 const selectType = document.getElementById('property-type');
 const selectPrice = document.getElementById('max-price');
 
-selectLocation.addEventListener('change', validate);
+$('#location').change(function() {
+  console.log(this.value)
+  validate(this.value);
+ 
+});
+  
+$('#property-type').change(function() {
+  validate(this.value);
+ 
+});
+
+$('#max-price').change(function() {
+  validate(this.value);
+ 
+});
+
+
+function validate(selected) {
+  
+  selected = this.value;
+  if (selected !=='none') {
+    console.log(selected);
+    
+  }
+   if (selected ==='none') {
+    console.log('false');
+  };
+  
+  
+  
+
+}
+
+
+
+
+selectLocation.addEventListener('change', validate(selectLocation));
 selectType.addEventListener('change', validate);
 selectPrice.addEventListener('change', validate);
 
-function validate() {
-if (
-selectLocation.value ==='none' || 
-selectType.value ==='none' || 
-selectPrice.value ==='none') { 
-  $('#btn').click(function(event){
-    event.preventDefault();
-    console.log('prevented default')
-    console.log(selectLocation.value)
-    
-  });
-}
+// function validate(check) {
 
- if (selectLocation.value ==='none') {
-  $('.alert').addClass('show')
-  
-} else {
-  $('.alert').removeClass('show')
-}
+//  if (check.value ==='none') {
+//   $('.alert').addClass('show')
+//   console.log('dupa')
+// } else {
+//   $('.alert').removeClass('show')
+//   console.log('dupa2')
+// }
 
-if (selectType.value ==='none') {
-  $('#propType').addClass('show')
-  
-} else {
-  $('#propType').removeClass('show')
-}
 
-if (selectPrice.value ==='none') {
-  $('#maxPrice').addClass('show')
-  
-} else {
-  $('#maxPrice').removeClass('show')
-}
+// }
+
+// $('#btn').click(validate(selectLocation));
 
 
 
-}
-
-$('#btn').click(validate);
-
-
-
- $("#submit-mail").click(function(){
-        let  email = $("#email").val(); 
-        if (!email) {
-          $('#email').attr('placeholder', 'Please enter email');
-        }
-        else if(email.indexOf("@") < 0 || email.indexOf(".") < 0){
-          $('.emailMsg').toggleClass('show');
-          $('#email').val('');
+//  $("#submit-mail").click(function(){
+//         let  email = $("#email").val(); 
+//         if (!email) {
+//           $('#email').attr('placeholder', 'Please enter email');
+//         }
+//         else if(email.indexOf("@") < 0 || email.indexOf(".") < 0){
+//           $('.emailMsg').toggleClass('show');
+//           $('#email').val('');
          
 
-$('#email').attr('placeholder', '');
-          } else  {
-            console.log(email);
-            $('#email').val('Thank you!');
-            $('.emailMsg').toggleClass('show');
-          }
+// $('#email').attr('placeholder', '');
+//           } else  {
+//             console.log(email);
+//             $('#email').val('Thank you!');
+//             $('.emailMsg').toggleClass('show');
+//           }
         
   
         
       
-    });
+//     });
+
+function mailValidate() {
+  let mailInput = $('#email').val();
+  console.log(mailInput);
+  $('#mailMessage').text("das");
+}
+
+
+
+
+$('#submit-mail').click(function() {
+mailValidate();
+
+
+})
 
 
 
